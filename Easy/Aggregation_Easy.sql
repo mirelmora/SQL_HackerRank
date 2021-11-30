6c1b06d49f0027f99c8d98ea953f578f81556550
Mirel Mora <mirel.mora@gmail.com>
2021-11-30 18:37:24 +0100

    Update Aggregation_Easy.sql

--- Easy/Aggregation_Easy.sql
+++ Easy/Aggregation_Easy.sql
@@ -7,3 +7,23 @@ WHERE DISTRICT = 'California'
 /* Query a count of the number of cities in CITY having a Population larger than */
 SELECT COUNT(POPULATION) FROM CITY
 WHERE POPULATION > 100000
+
+/* Average Population */
+/* Query the average population for all cities in CITY, rounded down to the nearest integer. */
+SELECT ROUND(AVG(POPULATION)) FROM CITY
+
+/* Japan Population */
+/* Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */
+SELECT SUM(POPULATION) FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+
+/*  Population Density Difference */
+/* Query the difference between the maximum and minimum populations in CITY.  */
+SELECT MAX(POPULATION)- MIN(POPULATION) AS DIFFERENCE
+FROM CITY
+
+/* The Blunder */
+/* Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.  */
+
+SELECT CEIL((AVG(SALARY)) - (AVG(REPLACE((SALARY), '0', '')))) AS AVG_SALARY FROM EMPLOYEES
